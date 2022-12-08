d42c9704c155020f2e512938c150a979081a72f2
andrematias <andre.matias@logicc.co.uk>
2022-12-08 13:49:13 +0000

    Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState("2022");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  console.log(filteredExpenses.lenght);
+
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangedFilter={filterChangeHandler}/>
-
-      {filteredExpenses.map((expense) => 
-        <ExpenseItem
-          key={expense.id} 
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangedFilter={filterChangeHandler}
+      />
+      oi
+      {filteredExpenses.lenght === 0 ? (
+        <p>No expenses found.</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
       )}
-
     </Card>
   );
-}
+};
 
 export default Expenses;
